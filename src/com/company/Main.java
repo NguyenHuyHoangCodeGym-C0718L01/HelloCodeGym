7c73b30ed2f4360fe70c12e2db77b2db06f314da
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-08-17 18:59:26 +0700

    Hoang finished HelloCodeGym Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Dear anh/chị");
+        System.out.println("Em tên là Nguyễn Huy Hoàng lớp CGC 3 Java C0718L01");
+        System.out.println("Hello CodeGym");
+    }
+}
